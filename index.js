8211344b54ec24f6f16907114f76089525e362db
Muhammad Furqan <furqan.khanzada@gmail.com>
2017-06-02 16:18:29 +0500

    ini

---
+++ index.js
@@ -0,0 +1,15 @@
+// Type Safety
+var test = 'Hello World';
+//let test: number = 5;
+//let test: boolean = true;
+//let test: any = {};
+//let test: string | number = 444;
+var testArr = ["asdas", 'Two'];
+//let testObj : string[] = new Array()
+function add(numOne, numTwo) {
+    numOne + numOne;
+}
+console.log("" + test);
+console.log(test.length);
+console.log(typeof test);
+console.log(add(5, 5));
---
+++ index.ts
@@ -0,0 +1,34 @@
+// Type Safety
+
+let test: string = 'Hello World';
+//let test: number = 5;
+//let test: boolean = true;
+//let test: any = {};
+//let test: string | number = 444;
+let testArr : [string, number] = ["asdas", 2];
+//let testObj : string[] = new Array()
+
+function add(numOne: number, numTwo: number) : void {
+      numOne + numOne
+}
+
+console.log(`${test}`);
+console.log(test.length);
+console.log(typeof test);
+console.log(add(5, 5));
+
+
+
+
+
+
+
+
+
+// Password: @ppbakerzzong50
+
+// Optional params [mail_id?:string], default value [param2[:type] = default_value], rest params
+
+
+// git status
+// git init
