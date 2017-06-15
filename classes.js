2452b5776511059a7d1e70bb70c6e27e8b9f714f
Muhammad Furqan <furqan.khanzada@gmail.com>
2017-06-15 16:52:32 +0500

    15 June 2017

--- classes.js
+++ classes.js
@@ -1,16 +1,67 @@
-var Human = (function () {
-    function Human(name) {
-        this.setName(name);
-    }
-    Human.prototype.setName = function (name) {
-        this.name = name;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    Human.prototype.getName = function () {
-        return this.name;
+})();
+var Human = (function () {
+    function Human(fName, lName) {
+        // let _self = this;
+        // setTimeout(function(){
+        //     _self.setFulName(fName, lName)
+        // }, 2000);
+        var _this = this;
+        setTimeout(function () {
+            _this.setFulName(fName, lName);
+        }, 2000);
+    }
+    Human.prototype.setFulName = function (fName, lName) {
+        this.fullName = fName + " " + lName;
+    };
+    Human.prototype.getFullName = function () {
+        return this.fullName;
     };
     return Human;
 }());
-var h = new Human('Furqan');
-console.log(' h :', h.getName());
-//h.eat() 
+var h = new Human('Furqan', 'Khanzada');
+console.log(' h :', h.getFullName());
+console.log(' fname :', h.fullName);
+console.log(' instanceof :', h instanceof Human);
+setTimeout(function () {
+    console.log(' h :', h.getFullName());
+    console.log(' fname :', h.fullName);
+}, 2500);
+//h.eat()
+// TASK :
+// This class should have 2 props (firstName and lastName)
+// then make a method which will return full name and it should be getFullName
+var A = (function () {
+    function A() {
+        console.log('A');
+    }
+    A.prototype.hello = function () {
+        console.log('hello parent');
+    };
+    return A;
+}());
+var B = (function (_super) {
+    __extends(B, _super);
+    function B() {
+        var _this = this;
+        console.log('B');
+        _this = _super.call(this) || this;
+        _this.hello();
+        return _this;
+    }
+    B.prototype.hello = function () {
+        _super.prototype.hello.call(this);
+        console.log('hello child');
+    };
+    return B;
+}(A));
+var ab = new A();
 //# sourceMappingURL=classes.js.map
--- classes.ts
+++ classes.ts
@@ -1,21 +1,59 @@
 class Human {
-    name: string
-    age: number
-    constructor(name: string){
-        this.setName(name);
+    fullName: string
+    constructor(fName: string, lName: string){
+        // let _self = this;
+        // setTimeout(function(){
+        //     _self.setFulName(fName, lName)
+        // }, 2000);
+
+        setTimeout(() => {
+            this.setFulName(fName, lName)
+        }, 2000);
+
     }
-    setName(name: string){
-        this.name = name;
+    setFulName(fName: string, lName: string) : void {
+        this.fullName = `${fName} ${lName}`;
     }
-    getName() : string {
-        return this.name
+    getFullName() : string {
+        return this.fullName
     }
 }
 
-const h = new Human('Furqan')
-console.log(' h :',  h.getName());
+const h = new Human('Furqan', 'Khanzada')
+console.log(' h :',  h.getFullName());
+console.log(' fname :',  h.fullName);
+console.log(' instanceof :',  h instanceof Human);
+
+        setTimeout(function(){
+console.log(' h :',  h.getFullName());
+console.log(' fname :',  h.fullName);
+        }, 2500);
+
 //h.eat()
 
 // TASK :
 // This class should have 2 props (firstName and lastName)
 // then make a method which will return full name and it should be getFullName
+
+class A {
+    constructor(){
+        console.log('A')
+    }
+    hello() :void {
+        console.log('hello parent')
+    }
+}
+
+class B extends A {
+    constructor(){
+        console.log('B')
+        super()
+        this.hello()
+    }
+    hello() :void {
+        super.hello()
+        console.log('hello child')
+    }
+}
+
+let ab: B = new A();
